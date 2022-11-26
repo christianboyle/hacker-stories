c0dd4a78ad626a6afd53f01293c865b05aca875c
Christian Boyle <mail@christianboyle.com>
2022-11-26 11:54:11 -0600

    switch to typescript

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react'
+
+function getTitle(title: string) {
+  return title
+}
+
+function App() {
+  return (
+    <div>
+      <h1>Hello {getTitle('React')}</h1>
+
+      <label htmlFor='search'>Search: </label>
+      <input id='search' type='text' />
+    </div>
+  )
+}
+
+export default App
