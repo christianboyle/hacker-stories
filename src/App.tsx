8ec25fc56e986e456305dcd8057d6f6ebb83422e
Christian Boyle <mail@christianboyle.com>
2022-12-22 15:47:12 -0600

    move from client-side search to server-side search

--- src/App.tsx
+++ src/App.tsx
@@ -101,9 +101,11 @@ const App = () => {
   })
 
   React.useEffect(() => {
+    if (!searchTerm) return
+
     dispatchStories({ type: 'STORIES_FETCH_INIT' })
 
-    fetch(`${API_ENDPOINT}react`)
+    fetch(`${API_ENDPOINT}${searchTerm}`)
       .then((response) => response.json())
       .then((result) => {
         dispatchStories({
@@ -112,7 +114,7 @@ const App = () => {
         })
       })
       .catch(() => dispatchStories({ type: 'STORIES_FETCH_FAILURE' }))
-  }, [])
+  }, [searchTerm])
 
   const handleRemoveStory = (item: Story) => {
     dispatchStories({
@@ -149,7 +151,7 @@ const App = () => {
       {stories.isLoading ? (
         <p>Loading ...</p>
       ) : (
-        <List list={searchedStories} onRemoveItem={handleRemoveStory} />
+        <List list={stories.data} onRemoveItem={handleRemoveStory} />
       )}
     </div>
   )
