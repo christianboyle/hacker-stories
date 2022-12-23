4c082525c649f66d2d2d688d6f0a4a0da8cae0a5
Christian Boyle <mail@christianboyle.com>
2022-12-23 08:36:36 -0600

    implement handleFetchStories with useCallback

--- src/App.tsx
+++ src/App.tsx
@@ -100,7 +100,7 @@ const App = () => {
     isError: false
   })
 
-  React.useEffect(() => {
+  const handleFetchStories = React.useCallback(() => {
     if (!searchTerm) return
 
     dispatchStories({ type: 'STORIES_FETCH_INIT' })
@@ -115,6 +115,10 @@ const App = () => {
       })
       .catch(() => dispatchStories({ type: 'STORIES_FETCH_FAILURE' }))
   }, [searchTerm])
+
+  React.useEffect(() => {
+    handleFetchStories()
+  }, [handleFetchStories])
 
   const handleRemoveStory = (item: Story) => {
     dispatchStories({
