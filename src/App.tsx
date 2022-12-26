9f3a0743c312acefa43acdb57890d03c09253687
Christian Boyle <mail@christianboyle.com>
2022-12-26 09:15:40 -0600

    use async/await and move from then/catch to try/catch

--- src/App.tsx
+++ src/App.tsx
@@ -103,18 +103,18 @@ const App = () => {
     isError: false
   })
 
-  const handleFetchStories = React.useCallback(() => {
+  const handleFetchStories = React.useCallback(async () => {
     dispatchStories({ type: 'STORIES_FETCH_INIT' })
 
-    axios
-      .get(url)
-      .then((result) => {
-        dispatchStories({
-          type: 'STORIES_FETCH_SUCCESS',
-          payload: result.data.hits
-        })
+    try {
+      const result = await axios.get(url)
+      dispatchStories({
+        type: 'STORIES_FETCH_SUCCESS',
+        payload: result.data.hits
       })
-      .catch(() => dispatchStories({ type: 'STORIES_FETCH_FAILURE' }))
+    } catch {
+      dispatchStories({ type: 'STORIES_FETCH_FAILURE' })
+    }
   }, [url])
 
   React.useEffect(() => {
