2feb41c3bb3645a42ff2f0c70397651424a16ad9
Christian Boyle <mail@christianboyle.com>
2022-12-28 09:26:22 -0600

    implement styling with vanilla css

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react'
 import axios from 'axios'
 
+import './App.css'
+
 type Story = {
   title: string
   url: string
@@ -140,16 +142,14 @@ const App = () => {
   }
 
   return (
-    <div>
-      <h1>My Hacker Stories</h1>
+    <div className='container'>
+      <h1 className='headline-primary'>My Hacker Stories</h1>
 
       <SearchForm
         searchTerm={searchTerm}
         onSearchInput={handleSearchInput}
         onSearchSubmit={handleSearchSubmit}
       />
-
-      <hr />
 
       {stories.isError && <p>Something went wrong ...</p>}
 
@@ -173,7 +173,7 @@ const SearchForm: React.FC<SearchFormProps> = ({
   onSearchInput,
   onSearchSubmit
 }) => (
-  <form onSubmit={onSearchSubmit}>
+  <form onSubmit={onSearchSubmit} className='search-form'>
     <InputWithLabel
       id='search'
       value={searchTerm}
@@ -183,7 +183,11 @@ const SearchForm: React.FC<SearchFormProps> = ({
       <strong>Search:</strong>
     </InputWithLabel>
 
-    <button type='submit' disabled={!searchTerm}>
+    <button
+      type='submit'
+      disabled={!searchTerm}
+      className='button button_large'
+    >
       Submit
     </button>
   </form>
@@ -216,7 +220,9 @@ const InputWithLabel: React.FC<InputWithLabelProps> = ({
 
   return (
     <>
-      <label htmlFor={id}>{children}</label>
+      <label htmlFor={id} className='label'>
+        {children}
+      </label>
       &nbsp;
       <input
         ref={inputRef}
@@ -224,6 +230,7 @@ const InputWithLabel: React.FC<InputWithLabelProps> = ({
         type={type}
         value={value}
         onChange={onInputChange}
+        className='input'
       />
     </>
   )
@@ -248,15 +255,19 @@ type ItemProps = {
 }
 
 const Item: React.FC<ItemProps> = ({ item, onRemoveItem }) => (
-  <li>
-    <span>
+  <li className='item'>
+    <span style={{ width: '40%' }}>
       <a href={item.url}>{item.title}</a>
     </span>
-    <span>{item.author}</span>
-    <span>{item.num_comments}</span>
-    <span>{item.points}</span>
-    <span>
-      <button type='button' onClick={() => onRemoveItem(item)}>
+    <span style={{ width: '30%' }}>{item.author}</span>
+    <span style={{ width: '10%' }}>{item.num_comments}</span>
+    <span style={{ width: '10%' }}>{item.points}</span>
+    <span style={{ width: '10%' }}>
+      <button
+        type='button'
+        onClick={() => onRemoveItem(item)}
+        className='button button_small'
+      >
         Dismiss
       </button>
     </span>
