13b65a5903d63bf969acdc42ee406ade24e31286
Christian Boyle <mail@christianboyle.com>
2023-01-03 09:58:12 -0600

    implement reverse sort

--- src/App.tsx
+++ src/App.tsx
@@ -316,6 +316,7 @@ const InputWithLabel: React.FC<InputWithLabelProps> = ({
 type ListProps = {
   list: Stories
   onRemoveItem: (item: Story) => void
+  sortKey: number
 }
 
 const SORTS = {
@@ -326,15 +327,22 @@ const SORTS = {
   POINT: (list) => sortBy(list, 'points').reverse()
 }
 
-const List = ({ list, onRemoveItem }) => {
-  const [sort, setSort] = React.useState('NONE')
+const List: React.FC<ListProps> = ({ list, onRemoveItem }) => {
+  const [sort, setSort] = React.useState({
+    sortKey: 'NONE',
+    isReverse: false
+  })
 
   const handleSort = (sortKey) => {
-    setSort(sortKey)
+    const isReverse = sort.sortKey === sortKey && !sort.isReverse
+
+    setSort({ sortKey, isReverse })
   }
 
-  const sortFunction = SORTS[sort]
-  const sortedList = sortFunction(list)
+  const sortFunction = SORTS[sort.sortKey]
+  const sortedList = sort.isReverse
+    ? sortFunction(list).reverse()
+    : sortFunction(list)
 
   return (
     <ul>
